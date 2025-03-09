d2eef0c8863383945761254bd91604785f6f9707
optshj <optshj@gmail.com>
2025-03-09 15:01:05 +0900

    [Fix] SEO 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,14 +9,17 @@ import Providers from "@/store/Provider"
 
 const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600", "700"] })
 
+export const metadata: Metadata = {
+    title: "리멤북",
+    description: "리멤북으로 읽은 책을 기록해 보세요",
+    verification: {
+        google: "huSHYvBftuJFa028T6tGdShVcrWecu4qHuQWqhyKrmU"
+    }
+}
+
 export default function RootLayout({ children, modal }: Readonly<{ children: React.ReactNode; modal: React.ReactNode }>) {
     return (
         <html lang="en">
-            <Head>
-                <title>{"리멤북"}</title>
-                <meta name="description" content={"리멤북으로 읽은 책을 기록해 보세요"} />
-                <meta name="google-site-verification" content="huSHYvBftuJFa028T6tGdShVcrWecu4qHuQWqhyKrmU" />
-            </Head>
             <body className={inter.className}>
                 <Providers>
                     <Header />
