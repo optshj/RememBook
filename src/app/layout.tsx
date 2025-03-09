2829f454ae68cfa6c0ba2134334c5ea38fe75b2a
optshj <optshj@gmail.com>
2025-03-09 14:57:13 +0900

    [Feat] SEO 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
@@ -8,14 +9,14 @@ import Providers from "@/store/Provider"
 
 const inter = Inter({ subsets: ["latin"], weight: ["400", "500", "600", "700"] })
 
-export const metadata: Metadata = {
-    title: "리멤북",
-    description: "리멤북으로 읽은 책을 기록해 보세요"
-}
-
 export default function RootLayout({ children, modal }: Readonly<{ children: React.ReactNode; modal: React.ReactNode }>) {
     return (
         <html lang="en">
+            <Head>
+                <title>{"리멤북"}</title>
+                <meta name="description" content={"리멤북으로 읽은 책을 기록해 보세요"} />
+                <meta name="google-site-verification" content="huSHYvBftuJFa028T6tGdShVcrWecu4qHuQWqhyKrmU" />
+            </Head>
             <body className={inter.className}>
                 <Providers>
                     <Header />
