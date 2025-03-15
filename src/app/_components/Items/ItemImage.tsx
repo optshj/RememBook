c5582e73a416dcc0af61aa9684c1354a84ca899f
optshj <optshj@gmail.com>
2025-03-15 20:19:23 +0900

    [Feat] 로고 추가

--- src/app/_components/Items/ItemImage.tsx
+++ src/app/_components/Items/ItemImage.tsx
@@ -30,7 +30,7 @@ export default function ItemImage({ book, state, className }: ItemImageProps) {
     }
 
     return (
-        <div className={`group relative ${className}`}>
+        <div className={`group relative ${className} `}>
             <Link href={`/information?isbn13=${book.isbn13}`}>
                 <Image src={book.cover} alt={book.title} className="cursor-pointer rounded-lg" quality={100} sizes="20vw" fill={true} loading="eager" />
             </Link>
@@ -41,7 +41,7 @@ export default function ItemImage({ book, state, className }: ItemImageProps) {
             <div className="absolute right-1 top-1 z-50 rounded-full" tabIndex={0} onClick={() => setIsOpen(prev => !prev)} onBlur={() => setIsOpen(false)}>
                 <HiOutlineDotsVertical className="h-10 w-10 cursor-pointer rounded-full p-2 text-white opacity-0 transition-opacity duration-300 hover:bg-[rgba(255,255,255,0.2)] group-hover:opacity-100" />
                 <ul
-                    className={`absolute right-0 top-full z-50 mt-2 w-52 rounded-lg bg-white font-semibold shadow-lg transition-all duration-300 ${isOpen ? "visible opacity-100" : "invisible opacity-0"}`}>
+                    className={`absolute right-0 top-full z-50 mt-2 w-40 rounded-lg bg-white font-semibold shadow-lg transition-all duration-300 ${isOpen ? "visible opacity-100" : "invisible opacity-0"}`}>
                     <li>
                         <Link
                             href={`/information?isbn13=${book.isbn13}`}
@@ -60,7 +60,7 @@ export default function ItemImage({ book, state, className }: ItemImageProps) {
                     </li>
                     <li className="flex cursor-pointer items-center gap-2 rounded-b-lg px-4 py-2 hover:bg-gray-100" onClick={deleteItem}>
                         <HiTrash />
-                        {"서재에서 삭제하기"}
+                        {"서재에서 삭제"}
                     </li>
                 </ul>
             </div>
