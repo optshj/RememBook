da7c095fc77a60c8f144811e7ceead9869b52162
optshj <optshj@gmail.com>
2025-03-21 16:19:09 +0900

    [Feat] 환경변수명 변경

--- src/app/api/aladin/querytype/route.tsx
+++ src/app/api/aladin/querytype/route.tsx
@@ -7,7 +7,7 @@ export async function POST(request: NextRequest) {
         const category = body.category
 
         const response = await fetch(
-            `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${process.env.NEXT_PUBLIC_ALADIN_TTB_KEY}&QueryType=${queryType}&cover=big&Version=20131101&SearchTarget=Book&output=js&MaxResults=20&CategoryId=${category}`
+            `http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=${process.env.ALADIN_TTB_KEY}&QueryType=${queryType}&cover=big&Version=20131101&SearchTarget=Book&output=js&MaxResults=20&CategoryId=${category}`
         )
         const data = await response.json()
         return NextResponse.json(data)
--- src/app/api/aladin/searchbookItem/route.tsx
+++ src/app/api/aladin/searchbookItem/route.tsx
@@ -5,7 +5,7 @@ export async function POST(request: NextRequest) {
         const body = await request.json()
         const isbn13 = body.isbn13
         const response = await fetch(
-            `http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey=${process.env.NEXT_PUBLIC_ALADIN_TTB_KEY}&ItemId=${isbn13}&ItemIdType=ISBN13&cover=big&output=js`
+            `http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey=${process.env.ALADIN_TTB_KEY}&ItemId=${isbn13}&ItemIdType=ISBN13&cover=big&output=js`
         )
         let data = await response.text()
         data = data.replace(/\\[abfnrtv0'"\\]/g, "")
